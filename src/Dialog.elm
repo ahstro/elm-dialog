7bf88ccc84f132af4aa9b85cfa3af54274f611e2
Anton Strömkvist <anton@stromkvist.com>
2018-06-12 17:29:44 +0200

    Replace header/body/footer with content
    Instead of taking in `Maybe`s for `header`, `body`, and `footer`,
    take a single `content: Html msg`. This is nice in a few ways.
    
    First, it Makes Impossible States Impossible, in that we can now never
    have an empty modal (`Nothing`s for `header`, `body`, and `footer`)
    unless the user specifically sends in an empty `Html msg` as `content`.
    
    Secondly, it mostly decouples the styling from Bootstrap, allowing the
    user to enter whatever kind of content they want, with whatever classes
    and styles they choose. You can still use Bootstrap, but you no longer
    "have" to.

--- src/Dialog.elm
+++ src/Dialog.elm
@@ -91,9 +91,8 @@ view maybeConfig =
                             [ empty ]
 
                         Just config ->
-                            [ wrapHeader config.closeMessage config.header
-                            , unwrap empty wrapBody config.body
-                            , unwrap empty wrapFooter config.footer
+                            [ unwrap empty closeButton config.closeMessage
+                            , config.content
                             ]
                     )
                 ]
@@ -102,33 +101,10 @@ view maybeConfig =
         ]
 
 
-wrapHeader : Maybe msg -> Maybe (Html msg) -> Html msg
-wrapHeader closeMessage header =
-    if closeMessage == Nothing && header == Nothing then
-        empty
-    else
-        div [ class "modal-header" ]
-            [ unwrap empty closeButton closeMessage
-            , Maybe.withDefault empty header
-            ]
-
-
 closeButton : msg -> Html msg
 closeButton closeMessage =
     button [ class "close", onClick closeMessage ]
         [ text "x" ]
-
-
-wrapBody : Html msg -> Html msg
-wrapBody body =
-    div [ class "modal-body" ]
-        [ body ]
-
-
-wrapFooter : Html msg -> Html msg
-wrapFooter footer =
-    div [ class "modal-footer" ]
-        [ footer ]
 
 
 backdrop : Maybe (Config msg) -> Html msg
@@ -155,9 +131,7 @@ clicks the 'X' in the top right. If you don't want that X displayed, use `Nothin
 type alias Config msg =
     { closeMessage : Maybe msg
     , containerClass : Maybe String
-    , header : Maybe (Html msg)
-    , body : Maybe (Html msg)
-    , footer : Maybe (Html msg)
+    , content : Html msg
     }
 
 
@@ -169,9 +143,7 @@ map : (a -> b) -> Config a -> Config b
 map f config =
     { closeMessage = Maybe.map f config.closeMessage
     , containerClass = config.containerClass
-    , header = Maybe.map (Html.map f) config.header
-    , body = Maybe.map (Html.map f) config.body
-    , footer = Maybe.map (Html.map f) config.footer
+    , content = Html.map f config.content
     }
 
 
